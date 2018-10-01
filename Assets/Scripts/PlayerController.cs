fca8e98180d203396729425718f769cc336b286b
Guillaume Lavoie-Harvey <glavoieh@gmail.com>
2018-10-01 15:41:59 -0400

    initial work on tp1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInputs();
+	}
+
+    private void HandleInputs()
+    {
+        rb.AddForce(transform.right * Input.GetAxis("Horizontal") * 20f);
+    }
+
+}
